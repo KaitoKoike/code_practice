77553f4d5bb5b4df297031d369764de183229408
Kaito Koike <kaikai.poteti@keio.jp>
2018-04-10 19:16:40 +0900

    ""

--- yukicoder/No.365(ジェンガソート).py
+++ yukicoder/No.365(ジェンガソート).py
@@ -2,18 +2,11 @@
 number_block = int(input())
 blocks = list(map(int,input().split()))
 
-max_length = 0
-move_index = 0
+number_right_order = number_block
 
-for block in blocks:
-    if block > max_length:
-        max_length = block
-    else:
-        move_index = max(move_index,block)
+for block in blocks[::-1]:
+    if number_right_order == block:
+        number_right_order -= 1
 
-ans = 0
 
-for block in blocks:
-    if block <= move_index:
-        ans += 1
-print(ans)
+print(number_right_order)

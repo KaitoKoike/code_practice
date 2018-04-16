aa773580b86f2d3b456d15f7182b397392eec4a2
Kaito Koike <kaikai.poteti@keio.jp>
2018-04-16 23:45:54 +0900

    ""

--- Atcoder/ABC/094/D.py
+++ Atcoder/ABC/094/D.py
@@ -1,12 +1,12 @@
 from decimal import *
 n = int(input())
 
-number_list = list(int,map(input().split()))
+number_list = list(map(int,input().split()))
 number_list.sort()
 max_number = max(number_list)
 middle_number = Decimal(max_number) / Decimal(2)
 for i in range(len(number_list)-1):
-    if (number_list[i]-middle_number)*(number_list[i+1]-middle_number) < 0:
+    if (number_list[i]-float(middle_number))*(number_list[i+1]-float(middle_number)) < 0:
         if abs(number_list[i]-middle_number) <= abs(number_list[i+1]-middle_number):
             print(max_number,number_list[i])
             break
---
+++ yukicoder/No.671(1000000007).py
@@ -0,0 +1,5 @@
+n = input()
+input_number = n.count("0")
+right_number = "1000000007".count("0")
+
+print(abs(input_number-right_number))
